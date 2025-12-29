3cadd9a74b0c1ff683a1814395f57d15c93a8a8f
Arshia Nikoo <arsh.nikoosiar@gmail.com>
2025-12-29 21:59:36 +0330

    Update mikrotik-block.sh

--- mikrotik-block.sh
+++ mikrotik-block.sh
@@ -6,7 +6,7 @@ ACTION=$(echo "$INPUT_JSON" | jq -r '.command')
 SRCIP=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.data.srcaddr')
 LOGFILE="/var/ossec/logs/active-responses.log"
 MIKROTIK_USER="Wazuh"
-MIKROTIK_HOST="YOUR-MIKROTIK-IP"
+MIKROTIK_HOST=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.location')
 MIKROTIK_PORT="MIKROTIK-SSH-PORT"
 ADDRESS_LIST="wazuh-bruteforce"
 RULE_COMMENT="Drop-Bruteforce"
@@ -21,12 +21,9 @@ SSH_CMD="ssh \
  ${MIKROTIK_USER}@${MIKROTIK_HOST}"
 
 $SSH_CMD "/ip firewall address-list add list=$ADDRESS_LIST address=$SRCIP comment=Wazuh-Bruteforce timeout=1h" >> "$LOGFILE" 2>&1
-
-echo "$(date) - Debug: Raw output is '$RAW_OUTPUT', Cleaned output is '$CHECK_FILTER'" >> "$LOGFILE"
-
-if [[ -z "$CHECK_FILTER" ]] || [[ "$CHECK_FILTER" -eq 0 ]]; then
-    echo "$(date) - Filter rule not found (Count: $CHECK_FILTER). Creating drop rule..." >> "$LOGFILE"
-    $SSH_CMD "/ip firewall filter add chain=input src-address-list=$ADDRESS_LIST action=drop comment=\"$RULE_COMMENT\" place-before=0" >> "$LOGFILE" 2>&1
+if $SSH_CMD "/ip firewall filter print" | grep -q "$RULE_COMMENT"; then
+    echo "$(date) - Filter rule found. Skipping." >> "$LOGFILE"
 else
-    echo "$(date) - Filter rule already exists (Count: $CHECK_FILTER). Skipping." >> "$LOGFILE"
+    echo "$(date) - Filter rule NOT found. Creating drop rule..." >> "$LOGFILE"
+    $SSH_CMD "/ip firewall filter add chain=input src-address-list=$ADDRESS_LIST action=drop comment=\"$RULE_COMMENT\" place-before=0" >> "$LOGFILE" 2>&1
 fi
