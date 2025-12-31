817be24dd4e22464d2dc52520bce6afae0c7fbc5
Arshia Nikoo <arsh.nikoosiar@gmail.com>
2025-12-31 12:52:07 +0330

    Update mikrotik-block.sh

--- Active-Response/mikrotik-block.sh
+++ Active-Response/mikrotik-block.sh
@@ -5,15 +5,15 @@ read INPUT_JSON
 ACTION=$(echo "$INPUT_JSON" | jq -r '.command')
 SRCIP=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.data.srcaddr')
 LOGFILE="/var/ossec/logs/active-responses.log"
-MIKROTIK_USER="Wazuh"
+MIKROTIK_USER="wazuh"
 MIKROTIK_HOST=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.location')
-MIKROTIK_PORT="MIKROTIK-SSH-PORT"
+MIKROTIK_PORT="MIKROTIK_SSH_PORT"
 ADDRESS_LIST="wazuh-bruteforce"
 RULE_COMMENT="Drop-Bruteforce"
 RAW_OUTPUT=$($SSH_CMD "/ip firewall filter print count-only where comment=\"$RULE_COMMENT\"" 2>/dev/null)
 CHECK_FILTER=$(echo "$RAW_OUTPUT" | sed 's/\x1b\[[0-9;]*m//g' | tr -d '\r\n[:space:]')
 SSH_CMD="ssh \
- -i /var/ossec/.ssh/id_ed25519 \
+ -i /var/ossec/.ssh/mt_rsa \
  -o BatchMode=yes \
  -o StrictHostKeyChecking=no \
  -o UserKnownHostsFile=/dev/null \
@@ -21,9 +21,9 @@ SSH_CMD="ssh \
  ${MIKROTIK_USER}@${MIKROTIK_HOST}"
 
 $SSH_CMD "/ip firewall address-list add list=$ADDRESS_LIST address=$SRCIP comment=Wazuh-Bruteforce timeout=1h" >> "$LOGFILE" 2>&1
+
 if $SSH_CMD "/ip firewall filter print" | grep -q "$RULE_COMMENT"; then
     echo "$(date) - Filter rule found. Skipping." >> "$LOGFILE"
 else
     echo "$(date) - Filter rule NOT found. Creating drop rule..." >> "$LOGFILE"
     $SSH_CMD "/ip firewall filter add chain=input src-address-list=$ADDRESS_LIST action=drop comment=\"$RULE_COMMENT\" place-before=0" >> "$LOGFILE" 2>&1
-fi
