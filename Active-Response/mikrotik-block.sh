2d709f443587e59d3ed3e898912e4094a42bbe37
Arshia Nikoo <0xarshh@gmail.com>
2025-12-31 22:12:33 +0330

    Refactor mikrotik-block.sh for clarity and efficiency

--- Active-Response/mikrotik-block.sh
+++ Active-Response/mikrotik-block.sh
@@ -2,16 +2,13 @@
 
 read INPUT_JSON
 
-ACTION=$(echo "$INPUT_JSON" | jq -r '.command')
 SRCIP=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.data.srcaddr')
+MIKROTIK_HOST=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.location')
 LOGFILE="/var/ossec/logs/active-responses.log"
 MIKROTIK_USER="wazuh"
-MIKROTIK_HOST=$(echo "$INPUT_JSON" | jq -r '.parameters.alert.location')
 MIKROTIK_PORT="MIKROTIK_SSH_PORT"
 ADDRESS_LIST="wazuh-bruteforce"
 RULE_COMMENT="Drop-Bruteforce"
-RAW_OUTPUT=$($SSH_CMD "/ip firewall filter print count-only where comment=\"$RULE_COMMENT\"" 2>/dev/null)
-CHECK_FILTER=$(echo "$RAW_OUTPUT" | sed 's/\x1b\[[0-9;]*m//g' | tr -d '\r\n[:space:]')
 SSH_CMD="ssh \
  -i /var/ossec/.ssh/mt_ed25519 \
  -o BatchMode=yes \
@@ -27,3 +24,4 @@ if $SSH_CMD "/ip firewall filter print" | grep -q "$RULE_COMMENT"; then
 else
     echo "$(date) - Filter rule NOT found. Creating drop rule..." >> "$LOGFILE"
     $SSH_CMD "/ip firewall filter add chain=input src-address-list=$ADDRESS_LIST action=drop comment=\"$RULE_COMMENT\" place-before=0" >> "$LOGFILE" 2>&1
+fi
